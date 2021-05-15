378cdb5a8861461ad8afedba2c55af6bb78603dd
Abishek Bashyal <abishek_bashyal@outlook.com>
2021-05-15 14:01:28 +0545

    vote done!

--- to-be-completed/vote.sol
+++ to-be-completed/vote.sol
@@ -5,36 +5,53 @@ contract cityPoll {
     struct City {
         string cityName;
         uint256 vote;
-        //you can add city details if you want
     }
 
 
-    mapping() public cities; //mapping city Id with the City ctruct - cityId should be uint256
-    mapping() hasVoted; //mapping to check if the address/account has voted or not
-
+    mapping(uint256 => City) public cities; 
+    mapping(address => bool) hasVoted; 
+    
     address owner;
     uint256 public cityCount = 0; // number of city added
+    
     constructor() public {
     
-    //TODO set contract caller as owner
-    //TODO set some intitial cities.
+        //TODO set contract caller as owner
+        owner = msg.sender;
+    
+        //TODO set some intitial cities.
+        addCity("Kathmandhu");
+        addCity("Pokhara");
+        
     }
  
  
-    function addCity() public {
+    function addCity(string memory cityName) public {
       //  TODO: add city to the CityStruct
+      require(msg.sender == owner,"only owner user can add cities");
+      
+      
+      City memory newCity = City({cityName:cityName, vote:0});
+      cities[cityCount] = newCity;
+      cityCount += 1;
+    }
+    
+    function vote(uint256 id) public {
+        //TODO Vote the selected city through cityID
+        require(hasVoted[msg.sender] == false,"user have already voted");
+        hasVoted[msg.sender] = true;
+        cities[id].vote += 1;
 
     }
     
-    function vote() public {
-        
-        //TODO Vote the selected city through cityID
-
+    function getCity(uint256 id) public view returns (string memory) {
+        // TODO get the city details through cityID
+        return cities[id].cityName;
     }
-    function getCity() public view returns (string memory) {
-     // TODO get the city details through cityID
-    }
-    function getVote() public view returns (uint256) {
-    // TODO get the vote of the city with its ID
+    
+    function getVote(uint256 id) public view returns (uint256) {
+        // TODO get the vote of the city with its ID
+        return cities[id].vote;
     }
 }
+
