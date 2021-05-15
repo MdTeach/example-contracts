20ddd4fdb196d2d44611ae411cea1e3c52068680
Abishek Bashyal <abishek_bashyal@outlook.com>
2021-05-15 12:50:26 +0545

    poe 2 done

--- to-be-completed/ProofOfExistence2.sol
+++ to-be-completed/ProofOfExistence2.sol
@@ -7,46 +7,39 @@ contract ProofOfExistence2 {
 
   // store a proof of existence in the contract state
   // *transactional function*
-  function storeProof(bytes32 proof) 
-    public 
-  {
-  
+  function storeProof(bytes32 proof) public {
+    if (!hasProof(proof)){
+        proofs.push(proof);
+    }
   }
 
   // calculate and store the proof for a document
   // *transactional function*
-  function notarize(string calldata document) 
-    external 
-  {
-
+  function notarize(string calldata document) external {
+    bytes32 proof = proofFor(document);
+    storeProof(proof);
   }
 
   // helper function to get a document's sha256
   // *read-only function*
-  function proofFor(string memory document) 
-    pure 
-    public 
-    returns (bytes32) 
-  {
-    
+  function proofFor(string memory document) pure public returns (bytes32) {
+    return sha256(abi.encodePacked(document));
   }
 
   // check if a document has been notarized
   // *read-only function*
-  function checkDocument(string memory document) 
-    public 
-    view 
-    returns (bool) 
-  {
-    
+  function checkDocument(string memory document) public view returns (bool) {
+    bytes32 proof = proofFor(document);
+    return hasProof(proof);
   }
 
   // returns true if proof is stored
   // *read-only function*
-  function hasProof(bytes32 proof) 
-    internal 
-    view 
-    returns (bool) 
-  {
+  function hasProof(bytes32 proof) internal view returns (bool) {
+    for (uint j = 0; j < proofs.length; j++) {
+      if(proof == proofs[j]) return true;
+    }
+    return false;
+  }
    
 }
